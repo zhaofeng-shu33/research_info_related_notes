6130fc973f6daf9c82a26c05ce3c3b192c4a31c4
zhaofeng-shu33 <616545598@qq.com>
2022-02-13 10:41:17 +0800

    add experiments

--- convex_hull/verification.py
+++ convex_hull/verification.py
@@ -1,10 +1,10 @@
 import argparse
-from ast import arg
 import numpy as np
+from scipy.optimize import fsolve
 from scipy.spatial import ConvexHull
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
-nTrial = 1000
+NTRIAL = 1000
 DISTRIBUTION = 'unit_circle'
 
 def random_points_2d_cauchy(n):
@@ -13,6 +13,18 @@ def random_points_2d_cauchy(n):
     theta = np.random.random(n)
     theta *= 2 * np.pi
     return np.vstack((r * np.cos(theta), r * np.sin(theta))).T
+
+def random_points_3d_cauchy(n):
+    y = np.random.random(n)
+    r = np.zeros(n)
+    for i in range(n):
+        f = lambda r: (-r/(2*(r*r+1))+np.arctan(r)/2)*4/np.pi - y[i]
+        r[i] = fsolve(f, [1.0])[0]
+    phi = 2 * np.pi * np.random.random(n)
+    theta = np.pi * np.random.random(n)
+    return np.vstack((r * np.cos(phi) * np.sin(theta),
+                      r * np.sin(theta) * np.sin(theta),
+                      r * np.cos(theta))).T
 
 def random_points_in_unit_circle(n):
     r = np.random.random(n)
@@ -26,18 +38,18 @@ def transform(n_list):
         return n_list ** (1/3)
     elif DISTRIBUTION == 'gaussian':
         return np.sqrt(np.log(n_list))
-    elif DISTRIBUTION == 'cauchy':
-        return n_list
     else:
-        raise ValueError("")
+        return n_list
 
 def countVertex(n):
     if DISTRIBUTION == 'unit_circle':
         points = random_points_in_unit_circle(n)
     elif DISTRIBUTION == 'gaussian':
         points = np.random.normal(size=(n, 2))
-    elif DISTRIBUTION == 'cauchy':
+    elif DISTRIBUTION == '2d-cauchy':
         points = random_points_2d_cauchy(n)
+    elif DISTRIBUTION == '3d-cauchy':
+        points = random_points_3d_cauchy(n)
     else:
         raise ValueError("")
     hull = ConvexHull(points)
@@ -51,24 +63,25 @@ def testAllN(n_list):
     nN = len(n_list)
     result = np.zeros(nN)
     for i, n in enumerate(n_list):
-        result[i] = np.mean(testN(n, nTrial))
+        result[i] = np.mean(testN(n, NTRIAL))
     return result
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--distribution',
-        choices=['unit_circle', 'gaussian', 'cauchy'], default='unit_circle')
+        choices=['unit_circle', 'gaussian', '2d-cauchy', '3d-cauchy'], default='unit_circle')
     args = parser.parse_args()
     DISTRIBUTION = args.distribution
     n_list = np.array(range(5, 100, 5))
     result = testAllN(n_list)
-    model = LinearRegression()
     transformed_n_list = transform(n_list)
-    reg = model.fit(transformed_n_list.reshape(-1, 1), result)
-    print(reg.coef_, reg.intercept_)
-    print(reg.score(transformed_n_list.reshape(-1, 1), result))
+    if args.distribution.find('cauchy') < 0:
+        model = LinearRegression()        
+        reg = model.fit(transformed_n_list.reshape(-1, 1), result)
+        print(reg.coef_, reg.intercept_)
+        print(reg.score(transformed_n_list.reshape(-1, 1), result))
     plt.plot(transformed_n_list, result)
     plt.xlabel('N')
     plt.ylabel('$E(V_N)$')
-    plt.title('2d cauchy')
+    plt.title(args.distribution)
     plt.show()
