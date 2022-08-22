c0d4b252ce1bf8b68e051ee0c4eb4fa1a5b3b6f8
zhaofeng-shu33 <616545598@qq.com>
2022-08-22 13:54:58 +0800

    update

--- convex_hull/H_formula_verification.py
+++ convex_hull/H_formula_verification.py
@@ -14,11 +14,12 @@ for i in range(repeat_time):
     if d_ > x:
         allowed += 1
 
-print(allowed / repeat_time)
+sim = allowed / repeat_time
 
 # theoretical result
 # alpha = np.arccos((2 * y**2 - z**2 - x**2)/(z**2 - x**2))
 # beta = np.arctan(z / x / np.tan(alpha / 2))
 G = lambda y: np.arccos(x/y) * d * y * np.exp(-d / 2 * y**2)
 value = quad(G, x, np.inf)[0] * 2 / np.pi
-print(value)
+rel_err = np.abs(sim - value)
+print(rel_err)
